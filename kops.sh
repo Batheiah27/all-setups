4470bf117b7cceaa838b987e31c909e6d24edc81
Batheiah27 <143469116+Batheiah27@users.noreply.github.com>
2025-11-17 10:50:18 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,8 +11,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name rahamss.k8s.local --zones ap-south-1a --control-plane-image ami-0f918f7e67a3323f0 --control-plane-count=1 --control-plane-size t2.large --image ami-0f918f7e67a3323f0 --node-count=2 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+aws s3api create-bucket --bucket batheiah18g21.k8s.local --region ap-south-1
+aws s3api put-bucket-versioning --bucket batheiah18g21.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://batheiah18g21.k8s.local
+kops create cluster --name batheiah3.k8s.local --zones ap-south-1a --control-plane-image ami-03695d52f0d883f65 --control-plane-count=1 --control-plane-size t2.micro --image ami-03695d52f0d883f65 --node-count=2 --node-size t2.micro
+kops update cluster --name batheiah3.k8s.local --yes --admin
