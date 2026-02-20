306caaef776f62c6e08d514abe4ec92cce765c98
Batheiah27 <143469116+Batheiah27@users.noreply.github.com>
2026-02-20 20:54:50 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,6 @@
 
 #! /bin/bash
+yum install git maven java-1.8.0-openjdk -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
