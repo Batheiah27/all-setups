30988d7dfd4d608bb0c03a42832e71f761f2230c
Batheiah27 <143469116+Batheiah27@users.noreply.github.com>
2026-07-12 10:21:59 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,12 +3,10 @@
 sudo apt update -y
 sudo apt install -y openjdk-17-jdk
 java -version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+https://pkg.jenkins.io/rpm-stable/jenkins.repo
 
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo apt update -y
 sudo apt install -y jenkins
 sudo systemctl start jenkins
